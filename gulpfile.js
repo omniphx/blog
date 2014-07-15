0e453078cca71f43cc68037dd4c6ede26614e5b5
Matthew Mitchener <mattjmitchener@gmail.com>
2014-07-14 19:43:43 -0600

    Modifying gulp and gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,16 @@ gulp.task('concat', function() {
   	'./bower_components/angular/angular.js',
   	'./bower_components/angular-bootstrap/ui-bootstrap.js',
   	'./bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-  	'./app/assets/javascript/*.js'])
+  	'./app/assets/scripts/*.js'])
     .pipe(concat('built.js'))
-    .pipe(gulp.dest('./public/assets/javascript'))
+    .pipe(gulp.dest('./public/assets/scripts'))
     .pipe(notify('Concat complete'))
 });
 
 gulp.task('uglify', function(){
-  gulp.src('./public/assets/javascript/*.js')
+  gulp.src('./public/assets/scripts/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./public/assets/javascript/compress'))
+    .pipe(gulp.dest('./public/assets/scripts/compress'))
     .pipe(notify('Uglify complete'));
 });
 
@@ -42,21 +42,21 @@ gulp.task('css', function(){
 
 gulp.task('dev', function(){
 	gulp.watch('./app/assets/styles/*.less', ['less']);
-	gulp.watch('./app/assets/javascript/extra.js', ['concat']);
+	gulp.watch('./app/assets/scripts/extra.js', ['concat']);
 });
 
 gulp.task('prod', function(){
 	gulp.watch('./app/assets/styles/*.less', ['less']);
 	gulp.watch('./public/assets/styles/styles.css', ['css']);
-	gulp.watch('./app/assets/javascript/extra.js', ['concat']);
-	gulp.watch('./public/assets/javascript/*.js', ['uglify']);
+	gulp.watch('./app/assets/scripts/extra.js', ['concat']);
+	gulp.watch('./public/assets/scripts/*.js', ['uglify']);
 });
 
 gulp.task('watch', function(){
 	gulp.watch('./app/assets/styles/*.less', ['less']);
 	gulp.watch('./public/assets/styles/styles.css', ['css']);
-	gulp.watch('./app/assets/javascript/extra.js', ['concat']);
-	gulp.watch('./public/assets/javascript/*.js', ['uglify']);
+	gulp.watch('./app/assets/scripts/extra.js', ['concat']);
+	gulp.watch('./public/assets/scripts/*.js', ['uglify']);
 });
 
 gulp.task('default', ['watch']);
